f0de25d7f5ff4ae011addc2689d041b6cc6d24e4
sandisde <sandisde4@gmail.com>
2015-06-16 11:11:55 +0300

    Product list update

---
+++ receptes/public/units_new.php
@@ -0,0 +1,46 @@
+<?php require_once("../include/sessions.php"); ?>
+<?php require_once("../include/db_connection.php"); ?>
+<?php require_once("../include/functions.php"); ?>
+
+<?php include("../include/layouts/header.php"); ?>
+<?php 
+    $prod_id=$_GET['id'];
+    mysqli_set_charset($connection,"utf8");
+    $query = "SELECT * from Products ";
+    $query .= "WHERE Product_id ='$prod_id'";
+    $result = mysqli_query($connection, $query);
+        confirm_query($result);
+    $row = mysqli_fetch_assoc($result);
+
+?>
+
+
+<div id="main">
+    <div id="navigation"></div>
+    <div id="page">
+        <?php echo message(); ?>
+        <h2>Edit Units</h2>
+        <form name="form1" method="post" action="prod_update.php" accept-charset="UTF-8">
+            <p> <label>ID:</label>
+                <input type="text" name="id" id="id" value="<?php echo $row['Product_id'];?>" />
+            </p>        
+            <p> <label>Product name:</label>
+                <input type="text" name="ProductName" value="<?php echo $row['ProductName'];?>" />
+            </p>
+            <p> <label>Calories:</label>
+                <input type="text" name="Calories" value="<?php echo $row['Calories'];?>" /> 
+            </p>
+            <p>
+            <input type="submit" name="submit" value="Edit Product"/>
+            </p>
+             
+            </form>       
+        
+            <a href="product_list.php"> Cancel</a>
+    </div>
+</div>
+    <?php
+ // 5. Release returned data
+    mysqli_free_result($result);
+    ?>       
+ <?php include("../include/layouts/footer.php"); ?>
